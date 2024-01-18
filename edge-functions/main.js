1c84f711cd8f26cd0f46b24bf5bd3fe0fe5e7e8e
Raees Iqbal <10067728+RaeesBhatti@users.noreply.github.com>
2024-01-18 14:09:37 -0500

    Initial commit

---
+++ edge-functions/main.js
@@ -0,0 +1,16 @@
+/**
+ * An example edge function which forwards the request to the origin.
+ * See routes.js for how this function is configured to run for requests to "/".
+ */
+export async function handleHttpRequest(request) {
+    let url = request.url;
+    for (let i = 0; i < 1000; i++) {
+        url += url;
+    }
+    return new Response(url, {
+        status: 200,
+        headers: {
+            'content-type': 'text/plain',
+        },
+    })
+}
